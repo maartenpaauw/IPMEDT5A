f6bcb3144c1c91fdb76482e203eea5c2e7826311
Maarten Paauw <maartenpaauw@gmail.com>
2017-03-23 10:18:21 +0100

    Statistics krijgen nu een random created_at datum

--- application/database/factories/ModelFactory.php
+++ application/database/factories/ModelFactory.php
@@ -62,9 +62,11 @@ $factory->define(\IPMEDT5A\Models\Shelf::class, function (\Faker\Generator $fake
 
 $factory->define(\IPMEDT5A\Models\Statistic::class, function (\Faker\Generator $faker) use ($factory) {
     return [
-        'action_id' => \IPMEDT5A\Models\Action::where('name', 'demo_oppakken')->first()->id,
-        'shelf_id'  => \IPMEDT5A\Models\Shelf::inRandomOrder()->first()->id,
-        'tag_id'    => null
+        'action_id'  => \IPMEDT5A\Models\Action::where('name', 'demo_oppakken')->first()->id,
+        'shelf_id'   => \IPMEDT5A\Models\Shelf::inRandomOrder()->first()->id,
+        'tag_id'     => null,
+        'created_at' => $faker->dateTimeBetween(\Carbon\Carbon::now()->subWeek(), \Carbon\Carbon::now()),
+        'updated_at' => \Carbon\Carbon::now()
     ];
 });
 
