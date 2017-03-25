e0cff2fc1cc8ea1b9c126563a7084ffdf9cbf2d8
Maarten Paauw <maartenpaauw@gmail.com>
2017-03-25 19:00:31 +0100

    Chart js werkend

--- application/angular/src/app/components/opgepakt-vandaag/opgepakt-vandaag.component.ts
+++ application/angular/src/app/components/opgepakt-vandaag/opgepakt-vandaag.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js';
 
 @Component({
   selector: 'app-opgepakt-vandaag',
--- application/angular/src/app/modules/dashboard.module.ts
+++ application/angular/src/app/modules/dashboard.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from "@angular/core";
 import {RouterModule} from "@angular/router";
 import {CommonModule} from "@angular/common";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { ChartsModule } from 'ng2-charts';
 import {DashboardComponent} from "../components/dashboard/dashboard.component";
 import {LoginGuard} from "../guards/login.guard";
 import {OverzichtComponent} from "../components/overzicht/overzicht.component";
@@ -18,6 +19,7 @@ import {OpgepaktVandaagComponent} from "../components/opgepakt-vandaag/opgepakt-
     imports: [
         CommonModule,
         NgbModule,
+        ChartsModule,
         RouterModule.forChild([
             {
                 path: '',
