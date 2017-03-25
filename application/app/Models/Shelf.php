d85c85a9a3c62cd68aa6ca59d76e706b97574b11
Maarten Paauw <maartenpaauw@gmail.com>
2017-03-25 17:01:46 +0100

    Shelf route model binding werkt nu op mac adres

--- application/app/Models/Shelf.php
+++ application/app/Models/Shelf.php
@@ -6,10 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shelf extends Model
 {
+    /**
+     * @var array
+     */
     protected $with = ['demo'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function demo()
     {
         return $this->hasOne(Demo::class, 'id', 'demo_id');
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'mac_address';
     }
 }
