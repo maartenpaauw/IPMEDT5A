9766fafe6a7516645d1f6569f98b5681b321e9df
Maarten Paauw <maartenpaauw@gmail.com>
2017-03-28 09:14:20 +0200

    Naam in menu

--- application/angular/src/app/components/menu/menu.component.ts
+++ application/angular/src/app/components/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {LoginService} from "../../services/login/login.service";
 import {LoginGuard} from "../../guards/login.guard";
 import {Router} from "@angular/router";
+import {User} from "../../interfaces/user.interface";
 
 @Component({
   selector: 'app-menu',
@@ -11,6 +12,7 @@ import {Router} from "@angular/router";
 export class MenuComponent implements OnInit {
 
   public loggedIn: boolean;
+  public user: User;
 
   constructor(private loginService: LoginService,
               private router: Router) { }
@@ -21,6 +23,7 @@ export class MenuComponent implements OnInit {
     });
 
     this.loggedIn = LoginGuard.check();
+    this.user = <User>JSON.parse(localStorage.getItem('user'));
   }
 
   public logout(): void {
