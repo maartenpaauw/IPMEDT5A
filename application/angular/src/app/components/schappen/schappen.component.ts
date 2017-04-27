8ac1a5045103fb0d09b6ac8fa4efb5127280e55a
Maarten Paauw <maartenpaauw@gmail.com>
2017-04-27 17:08:15 +0200

    Realtime

--- application/angular/src/app/components/schappen/schappen.component.ts
+++ application/angular/src/app/components/schappen/schappen.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Title} from "@angular/platform-browser";
 import {ShelvesService} from "../../services/shelves/shelves.service";
 import {Shelf} from "../../interfaces/shelf.interface";
 import {AutoUnsubscribe} from "../../decorators/auto.unsubscribe.decorator";
 import {Subscription} from "rxjs/Subscription";
+import {IntervalObservable} from "rxjs/observable/IntervalObservable";
 
 @Component({
   selector: 'app-schappen',
@@ -11,23 +12,33 @@ import {Subscription} from "rxjs/Subscription";
   styleUrls: ['./schappen.component.scss']
 })
 @AutoUnsubscribe()
-export class SchappenComponent implements OnInit {
+export class SchappenComponent implements OnInit, AfterViewInit {
 
   public shelves: Array<Shelf>;
 
   private shelfSubscription: Subscription;
+  private observable: Subscription;
 
   constructor(private titleService: Title,
               private shelvesService: ShelvesService) { }
 
   ngOnInit() {
     this.titleService.setTitle("Schappen — IPMEDT5A");
+  }
 
+  ngAfterViewInit() {
+    this.getShelves();
+
+    this.observable = IntervalObservable.create(5000).subscribe(() => {
+      this.getShelves();
+    });
+  }
+
+  private getShelves(): void {
     this.shelfSubscription = this.shelvesService.getShelves().subscribe(
         (res: any) => {
           this.shelves = res.data;
         }
     );
   }
-
 }
--- application/angular/src/app/components/voorraad/voorraad.component.ts
+++ application/angular/src/app/components/voorraad/voorraad.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Title} from "@angular/platform-browser";
 import {ProductsService} from "../../services/products/products.service";
 import {Product} from "../../interfaces/product.interface";
 import {Subscription} from "rxjs/Subscription";
 import {AutoUnsubscribe} from "../../decorators/auto.unsubscribe.decorator";
+import {IntervalObservable} from "rxjs/observable/IntervalObservable";
 
 @Component({
   selector: 'app-voorraad',
@@ -11,23 +12,33 @@ import {AutoUnsubscribe} from "../../decorators/auto.unsubscribe.decorator";
   styleUrls: ['./voorraad.component.scss']
 })
 @AutoUnsubscribe()
-export class VoorraadComponent implements OnInit {
+export class VoorraadComponent implements OnInit, AfterViewInit {
 
   public products: Array<Product>;
 
   private productSubscription: Subscription;
+  private observable: Subscription;
 
   constructor(private titleService: Title,
               private productsService: ProductsService) { }
 
   ngOnInit() {
     this.titleService.setTitle("Voorraad — IPMEDT5A");
+  }
 
+  ngAfterViewInit() {
+    this.getProducts();
+
+    this.observable = IntervalObservable.create(5000).subscribe(() => {
+      this.getProducts();
+    });
+  }
+
+  private getProducts(): void {
     this.productSubscription = this.productsService.getProducts().subscribe(
         (res: Array<Product>) => {
           this.products = res;
         }
     );
   }
-
 }
